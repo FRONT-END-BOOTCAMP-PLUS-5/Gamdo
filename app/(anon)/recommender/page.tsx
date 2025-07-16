44f5bc9e90bedd5201cb399a888680c382e4cc8f
LeemJungHoon <xjqhdjq@naver.com>
2025-07-17 00:58:33 +0900

    design: 추천페이지 UI 수정

--- app/(anon)/recommender/page.tsx
+++ app/(anon)/recommender/page.tsx
@@ -1,3 +1,5 @@
+import { SiCoffeescript } from "react-icons/si";
+
 const RecommenderPage = () => {
   return (
     // 전체 wrap
@@ -5,9 +7,7 @@ const RecommenderPage = () => {
       {/* 상단텍스트 + 사용자 정보 카테고리 감싸는 div */}
       <div className="flex flex-col w-4/5 h-screen mx-auto border-2 border-blue-500">
         {/* 취향에 딱 맞춘 영화 */}
-        <div className="flex my-15 justify-center text-white text-5xl">
-          취향에 딱 맞춘 영화를 추천해드릴게요
-        </div>
+        <div className="flex my-15 justify-center text-white text-5xl">취향에 딱 맞춘 영화를 추천해드릴게요</div>
         {/* 날씨 + 사용자 정보 감싸는 섹션 */}
         <div className="flex-1 flex-col border-2 border-red-500">
           {/* 날씨 */}
@@ -15,11 +15,9 @@ const RecommenderPage = () => {
             {/* 날씨 왼쪽 섹션 */}
             <div className="flex flex-col m-5 p-10 border-2 border-blue-500 text-white">
               {/* 날씨 기본 정보 섹션 */}
-              <div className="mb-5">
+              <div className="flex-1 mb-5 pt-10">
                 <span className="flex justify-center text-5xl">25°</span>
-                <span className="flex justify-center text-1xl">
-                  서울특별시 구로구
-                </span>
+                <span className="flex justify-center text-1xl mt-2">서울특별시 구로구</span>
               </div>
               {/* 날씨 각종 정보 섹션 */}
               <div className="flex ">
@@ -44,32 +42,29 @@ const RecommenderPage = () => {
               </div>
               {/* 날씨 버튼 컴포넌트 */}
               <div className="flex h-1/2 justify-center border-2 border-yellow-500 items-center px-5 gap-2">
-                <button className="border-2 border-white rounded-lg px-4 py-2 hover:cursor-pointer">
-                  맑음
-                </button>
-                <button className="border-2 border-white rounded-lg px-4 py-2 hover:cursor-pointer">
-                  흐림
-                </button>
-                <button className="border-2 border-white rounded-lg px-4 py-2 hover:cursor-pointer">
-                  비
-                </button>
-                <button className="border-2 border-white rounded-lg px-4 py-2 hover:cursor-pointer">
-                  눈
-                </button>
-                <button className="border-2 border-white rounded-lg px-4 py-2 hover:cursor-pointer">
-                  우박
-                </button>
-                <button className="border-2 border-white rounded-lg px-4 py-2 hover:cursor-pointer">
-                  안개
-                </button>
+                <button className="border-2 border-white rounded-lg px-4 py-2 hover:cursor-pointer">맑음</button>
+                <button className="border-2 border-white rounded-lg px-4 py-2 hover:cursor-pointer">흐림</button>
+                <button className="border-2 border-white rounded-lg px-4 py-2 hover:cursor-pointer">비</button>
+                <button className="border-2 border-white rounded-lg px-4 py-2 hover:cursor-pointer">눈</button>
+                <button className="border-2 border-white rounded-lg px-4 py-2 hover:cursor-pointer">우박</button>
+                <button className="border-2 border-white rounded-lg px-4 py-2 hover:cursor-pointer">안개</button>
               </div>
             </div>
           </div>
           {/* 사용자 정보 */}
           <div className="flex h-1/2 border-2 text-white text-2xl border-yellow-500">
-            <div className="flex-1 flex-col border-2 border-red-500">감정</div>
-            <div className="flex-1 flex-col border-2 border-red-500">장르</div>
-            <div className="flex-1 flex-col border-2 border-red-500">시간</div>
+            {/* 여기서부터 유저 정보선택 컴포넌트 */}
+            {/* 차후에 시간되면 컴포넌트 분리하여 map으로 돌릴것 */}
+            <div className="flex-1 flex-col m-5 p-5 border-2 border-red-500">
+              <div className="inline-flex justify-center items-center bg-[#DEFFFD] rounded-xl size-12">
+                <SiCoffeescript color="4BBEAB" size={28} />
+              </div>
+              <div className="mt-5 pl-1 pb-4 border-b-2 border-white">감정</div>
+              {/* 7.17여기부터 */}
+              <button className="border-2 border-white rounded-lg px-4 py-2 hover:cursor-pointer">안개</button>
+            </div>
+            <div className="flex-1 flex-col m-5 p-5 border-2 border-red-500">장르</div>
+            <div className="flex-1 flex-col m-5 p-5 border-2 border-red-500">시간</div>
           </div>
         </div>
       </div>
