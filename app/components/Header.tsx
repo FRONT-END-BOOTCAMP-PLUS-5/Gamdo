6708bb4155b7851dadd8f3b724f9f7aea70027a3
soyeon park <psy_sott120@naver.com>
2025-07-17 15:56:04 +0900

    design: 헤더 디자인 수정, 페이지 경로 별 조건부 스타일 추가

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -32,29 +32,39 @@ export default function Header() {
   };
 
   return (
-    <header className="sticky top-0 z-10 w-full h-11 bg-gradient-to-b from-black to-[#1D1F28]">
+    <header className="sticky top-0 z-10 w-full h-16 bg-gradient-to-b from-black to-[#1D1F28]">
       <div className="h-full flex items-center justify-between px-6 max-w-7xl mx-auto">
         <div className="flex items-center gap-2">
           <Link href="/">
             <Image
               src="/assets/icons/logo.svg"
               alt="로고"
-              width={130}
-              height={32}
+              width={150}
+              height={40}
               className="mr-10"
             />
           </Link>
-          <nav className="flex-1 flex justify-center gap-8 text-sm text-gray-200">
-            <a href="#" className="hover:text-white">
+          <nav className="flex-1 flex justify-center gap-8 text-md text-gray-200 font-light">
+            <Link
+              href="/"
+              className={`${
+                pathname === "/" ? "font-semibold text-white" : ""
+              }`}
+            >
               추천
-            </a>
-            <Link href="/search" className="hover:text-white">
+            </Link>
+            <Link
+              href="/search"
+              className={`${
+                pathname === "/search" ? "font-semibold text-white" : ""
+              }`}
+            >
               검색
             </Link>
           </nav>
         </div>
         {isAuthenticated ? (
-          <>
+          <div className="flex items-center gap-4">
             <div className="text-white">
               <Link href="/mypage">
                 <Image
@@ -71,7 +81,7 @@ export default function Header() {
             >
               로그아웃
             </div>
-          </>
+          </div>
         ) : (
           <div className="text-white cursor-pointer whitespace-nowrap">
             <Link href="/auth/signin">로그인</Link>
