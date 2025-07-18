6f6e15d6581d9ee26e4bdbb0a8d453251908c2fb
김지나 <jina4066@naver.com>
2025-07-18 12:31:39 +0900

    feat: 반환되는 platform 데이터 별 이미지 매핑 파일 생성

---
+++ app/components/Platform.ts
@@ -0,0 +1,8 @@
+export const platformMapping = {
+  netflix: "/assets/images/netflix.svg",
+  disney: "/assets/images/disney-plus.svg",
+  tving: "/assets/images/tving.png",
+  wavve: "/assets/images/wavve.png",
+  watcha: "/assets/images/watcha.png",
+  appletv: "/assets/images/apple-tv.png",
+} as const;
